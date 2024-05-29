df8231821c8bbaec6e3ade957017e4c2bcb2eca7
Noface <166717111+noface1200@users.noreply.github.com>
2024-05-29 06:28:41 +0100

    Create topbar.js

---
+++ riphub/sub/topbar/topbar.js
@@ -0,0 +1,12 @@
+
+function Home() {
+    window.open("../../index.html", "_blank");
+}
+
+function Pinned() {
+    window.open("../pinned/index.html", "_blank");
+}
+
+function discord() {
+    window.open("https://discord.gg/HgfuNVw3", "_blank");
+}
